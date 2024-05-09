9376ad3d9e93b752a1be6595af2b97dcac884913
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2024-05-09 21:45:12 +0600

    exploring next.js 14

---
+++ app/about/layout.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function AboutLayout({children}) {
+  return (
+    <div>
+        <nav className='mt-6 mb-6'><Link href="/mission">Mission</Link> || <Link href="/vision">Vision</Link></nav>
+        {children}
+    </div>
+  )
+}
